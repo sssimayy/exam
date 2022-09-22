01598ff95a652fc8d1749ee31a8f5fb283be6f6f
Simay <simay95@hotmail.com>
2022-09-22 15:24:30 +0300

    CandidateController has been added

---
+++ exam/src/main/java/com/ekiptesin/exam/contoller/CandidateController.java
@@ -0,0 +1,53 @@
+package com.ekiptesin.exam.contoller;
+
+import com.ekiptesin.exam.entity.Candidate;
+import com.ekiptesin.exam.results.DataResult;
+import com.ekiptesin.exam.results.Result;
+import com.ekiptesin.exam.results.SuccessDataResult;
+import com.ekiptesin.exam.service.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/candidate")
+public class CandidateController {
+
+    private CandidateService candidateService;
+
+    @Autowired
+    public CandidateController(CandidateService candidateService) {
+        this.candidateService = candidateService;
+    }
+
+    @PostMapping("/add")
+    public Result addCandidate(@RequestBody Candidate candidate) {
+        return candidateService.add(candidate);
+    }
+
+    @GetMapping("/getAll")
+    public DataResult<List<Candidate>> getAll() {
+        return candidateService.getAll();
+    }
+
+    @GetMapping("/getCandidateById")
+    public SuccessDataResult<Boolean> findAllByIsActiveAndCompanyName(Long id) {
+        return this.candidateService.getCandidateById(id);
+    }
+
+    @DeleteMapping("/deleteCandidate/{id}")
+    public void deleteCandidateById(@PathVariable("id") Long id) {
+        this.candidateService.deleteCandidateById(id);
+    }
+
+    @PutMapping("/updateCandidate/{id}")
+    public Candidate updateCandidateById(@RequestBody Candidate candidate) {
+        return this.candidateService.updateCandidateById(candidate);
+    }
+
+    @PostMapping("/changeStatus")
+    public Result changeStatus(Long id) {
+        return this.candidateService.changeStatus(id);
+    }
+}
